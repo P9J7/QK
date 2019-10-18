9c9132d6187e9f86acce6a4261fedb1311ce6b54
p9j7 <1002684417@qq.com>
2019-10-18 18:13:20 +0800

    11

--- src/QuickSortChart.py
+++ src/QuickSortChart.py
@@ -1,6 +1,8 @@
 import numpy as np
 import time
 from matplotlib import pyplot as plt
+import sys
+
 
 
 def partition(arr, low, high):
@@ -25,6 +27,8 @@ def quickSort(arr, low, high):
         pi = partition(arr, low, high)
         quickSort(arr, low, pi - 1)
         quickSort(arr, pi + 1, high)
+    return arr
+
 
 def randPartition(arr, low, high):
     pivot = np.random.randint(high - low) + low
@@ -38,38 +42,39 @@ def randQuickSort(arr, low, high):
         pi = randPartition(arr, low, high)
         randQuickSort(arr, low, pi - 1)
         randQuickSort(arr, pi + 1, high)
+    return arr
 
 
 # arr = [10, 7, 8, 9, 1, 5]
-space = np.arange(0, 100000, 100000)
-spend = np.zeros(int (100000/100000))
-randSpend = np.zeros(int (100000/100000))
-for index,i in enumerate(space):
-    print(index)
-    arr = np.random.randint(0, 100, i)
-    randArr = arr.copy()
-    n = len(arr)
-    # startTime = time.time()
-    # quickSort(arr, 0, n - 1)
-    # endTime = time.time()
-    # spend[index] = endTime - startTime
-    randStartTime = time.time()
-    randQuickSort(randArr, 0, n - 1)
-    randEndTime = time.time()
-    randSpend[index] = randEndTime - randStartTime
-# arr = np.random.randint(0, 100, 50000)
-# print(arr)
-# n = len(arr)
-# startTime = time.time()
-# quickSort(arr, 0, n - 1)
-# endTime = time.time()
-# print("排序后的数组:")
-# for i in range(n):
-#     print("%d" % arr[i])
-# print("快速排序所用时间为 %s 秒" % (endTime - startTime))
-for i, j in zip(space, spend):
-    print(i, "个数组排序所花时间为", j, "秒")
-plt.plot(space, spend, label = 'QuickSort')
-plt.plot(space, randSpend, label='RandQuickSort')
-plt.legend()
-plt.show()
+def experiment(start, end, step):
+    space = np.linspace(start, end, step + 1, dtype=int)
+    print(space)
+    spend = np.zeros(step + 1)
+    print(spend)
+    randSpend = np.zeros(step + 1)
+    print(randSpend)
+    for index,i in enumerate(space):
+        print(index)
+        arr = np.random.randint(0, 100, i)
+        randArr = arr.copy()
+        n = len(arr)
+        startTime = time.clock()
+        quickSort(arr, 0, n - 1)
+        endTime = time.clock()
+        spend[index] = endTime - startTime
+        randStartTime = time.clock()
+        randQuickSort(randArr, 0, n - 1)
+        randEndTime = time.clock()
+        randSpend[index] = randEndTime - randStartTime
+    return space, spend, randSpend
+
+
+if __name__ == '__main__':
+    space, spend, randSpend = experiment(0, 100000, 5)
+    sys.setrecursionlimit(100000)
+    for i, j in zip(space, spend):
+        print(i, "个数组快速排序所花时间为", j, "秒")
+    plt.plot(space, spend, label = 'QuickSort')
+    plt.plot(space, randSpend, label='RandQuickSort')
+    plt.legend()
+    plt.show()
